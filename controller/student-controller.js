fc8b43d5457530f39b26ebf1452004e51ebf6c59
mercy-ndiritu <mewanjikundiritu@gmail.com>
2025-03-11 11:37:11 -0500

    refactor student data upload to supabase

--- controller/student-controller.js
+++ controller/student-controller.js
@@ -1,6 +1,23 @@
+import { supabase } from "../database/db.js"
+
 //upload students dTA TO DB
 export const uploadData = async (req, res) => {
-    const {name, reg} = req.body
+    const {name, regno, course} = req.body
 
-    res.status(200).json({name:name, reg:reg})
+    try {
+        const {data, error} = await supabase
+            .from('simple')
+            .insert([{name:name, regno:regno, course:course}])
+            .select()
+
+        if (error) {
+            return res.status(400).json({message: 'An error occured while uploading data', error: error.message})
+        }  
+
+        return res.status(201).json({message: 'Data uploaded successfully', data})
+            
+    } catch (error) {
+        console.log(error.message)
+        throw new Error('An error occured while uploading data')
+    }
 }
